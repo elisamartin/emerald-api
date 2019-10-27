8fab7ea64bb5e9c2496d60c20e78f2f7fb63fd91
elisamartin <elisamv93@gmail.com>
2019-10-27 16:01:29 +0100

    Listing details added

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -4,7 +4,15 @@ import { connect } from 'react-redux';
 const Listing = ({ listing }) => {
   return (
     <div>
-      <p>{listing.title}</p>
+      <div className='listing-img' />
+      <div className='listing-text'>
+        <p>{listing.title}</p>
+        <p>{listing.city}</p>
+        <p>
+          Up to {listing.max_adults} guests, {listing.number_of_bedrooms} bedrooms
+        </p>
+        <p>from {listing.property_price}/night</p>
+      </div>
     </div>
   );
 };
