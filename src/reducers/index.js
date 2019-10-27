f80b63d8f4c163d1e1fb57f8da734bce9f1ff249
elisamartin <elisamv93@gmail.com>
2019-10-27 11:03:59 +0100

    Redux setup

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+const initState = {
+  listings: []
+};
+
+const listingReducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default listingReducer;
